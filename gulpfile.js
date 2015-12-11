9c60aba8ce4e063584d6f46c6d633ddb207ceaf7
Gregory Laubenstein <greg@Gregorys-MacBook-Pro.local>
2015-12-11 12:58:39 -0600

    add coveralls to build

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
+var istanbul = require('gulp-istanbul');
 var mocha = require('gulp-mocha');
 
-gulp.task('test', function () {
-    
+gulp.task('init-istanbul', function () {
+    return gulp.src(['twitter.js'])
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire());
+})
+
+gulp.task('test', ['init-istanbul'], function () {
+
     return gulp.src(['./test/*.js'], { read: false })
-        .pipe(mocha({ reporter: 'nyan' }));
+        .pipe(mocha({ reporter: 'nyan' }))
+        .pipe(istanbul.writeReports());
 });
